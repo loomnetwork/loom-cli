651eb59ea83fba1ba64f8f4f29959d2762363f17
Georgios Konstantopoulos <me@gakonst.com>
2019-03-10 21:48:40 +0200

    add config for the devnet

---
+++ src/trudy_oracle_devnet.ts
@@ -0,0 +1,12 @@
+export const config = {
+  ethPrivateKey:
+    "0xDE03930EA2DAB535A1717BE8B5249E14388F0B9836DE8CF3C9299AEB25EDAD98",
+    ethEndpoint: "https://rinkeby.infura.io/5Ic91y0T9nLh6qUg33K0",
+  dappchainPrivateKey:
+  "POcupxN+lMdekrgztzNtLOiyT0UUs+UdkQiC/lRKV4kIRQy+j86BSajcfZ+EG5WXhMHdsKf5DU3oRL+SjC68Nw==",
+  dappchainEndpoint: "http://13.232.228.220:46658",
+  loomTokenEthAddress: "0x88aEefFAe4c4Ad009cDaDdcE52A4373b2F026af4",
+  loomGatewayEthAddress: "0x426de37D73B0B768648335A5e1243e46b3a5E0A9",
+  validatorManagerEthAddress: "0xfba125a08a1e93bf814dbcab57aa9a2f811b8839",
+  chainId: "dev-oracle"
+};
