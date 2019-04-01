30b6984489379fc88d3cc7cadcf8a759e00b64f3
Georgios Konstantopoulos <me@gakonst.com>
2019-04-01 16:01:34 +0300

    use config file passed by cli to connect

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import program from "commander";
 import BN from "bn.js";
 
 import { DPOSUser, CryptoUtils, GatewayVersion } from "loom-js";
-import { config } from "./trudy_oracle_devnet";
 import { ICandidate } from "loom-js/dist/contracts/dpos";
 
 // See https://loomx.io/developers/docs/en/testnet-plasma.html#contract-addresses-transfer-gateway
@@ -13,20 +12,31 @@ import { ICandidate } from "loom-js/dist/contracts/dpos";
 const coinMultiplier = new BN(10).pow(new BN(18))
 
 program
+    .version('0.1.0')
+    .option('-c, --config <path>', 'config file absolute path')
+    .parse(process.argv)
+
+const config = require(program.config)
+
+const createUser = async (config: any) : Promise<DPOSUser> => {
+    return DPOSUser.createOfflineUserAsync(
+        config.ethEndpoint,
+        config.ethPrivateKey,
+        config.dappchainEndpoint,
+        config.dappchainPrivateKey,
+        config.chainId,
+        config.loomGatewayEthAddress,
+        GatewayVersion.SINGLESIG
+    );
+}
+
+program
   .command("deposit <amount>")
   .description(
     "deposit the specified amount of LOOM tokens into the Transfer Gateway"
   )
   .action(async function(amount: string) {
-    const user = await DPOSUser.createOfflineUserAsync(
-      config.ethEndpoint,
-      config.ethPrivateKey,
-      config.dappchainEndpoint,
-      config.dappchainPrivateKey,
-      config.chainId,
-      config.loomGatewayEthAddress,
-      GatewayVersion.SINGLESIG
-    );
+    const user = await createUser(config)
     try {
       const tx = await user.depositAsync(new BN(amount).mul(coinMultiplier));
       await tx.wait();
@@ -47,17 +57,8 @@ program
     "Number of seconds to wait for withdrawal to be processed"
   )
   .action(async function(amount: string, options: any) {
-    let user;
+    const user = await createUser(config)
     try {
-      user = await DPOSUser.createOfflineUserAsync(
-        config.ethEndpoint,
-        config.ethPrivateKey,
-        config.dappchainEndpoint,
-        config.dappchainPrivateKey,
-        config.chainId,
-        config.loomGatewayEthAddress,
-        GatewayVersion.SINGLESIG
-      );
       const actualAmount = new BN(amount).mul(coinMultiplier);
       const tx = await user.withdrawAsync(actualAmount);
       await tx.wait();
@@ -74,17 +75,8 @@ program
   .command("resume-withdrawal")
   .description("Resumes a withdrawal from a pending receipt")
   .action(async function() {
-    let user;
+    const user = await createUser(config)
     try {
-      user = await DPOSUser.createOfflineUserAsync(
-        config.ethEndpoint,
-        config.ethPrivateKey,
-        config.dappchainEndpoint,
-        config.dappchainPrivateKey,
-        config.chainId,
-        config.loomGatewayEthAddress,
-        GatewayVersion.SINGLESIG
-      );
       const tx = await user.resumeWithdrawalAsync();
       if (tx) {
         await tx.wait();
@@ -92,9 +84,7 @@ program
     } catch (err) {
       console.error(err);
     } finally {
-      if (user) {
         user.disconnect();
-      }
     }
   });
 
@@ -102,15 +92,7 @@ program
   .command("receipt")
   .description("Returns the currently pending receipt")
   .action(async function() {
-    const user = await DPOSUser.createOfflineUserAsync(
-      config.ethEndpoint,
-      config.ethPrivateKey,
-      config.dappchainEndpoint,
-      config.dappchainPrivateKey,
-      config.chainId,
-      config.loomGatewayEthAddress,
-        GatewayVersion.SINGLESIG
-    );
+    const user = await createUser(config)
     try {
       const receipt = await user.getPendingWithdrawalReceiptAsync();
       if (receipt) {
@@ -138,21 +120,12 @@ program
   .command("map-accounts")
   .description("Connects the user's eth/dappchain addresses")
   .action(async function() {
-    const user = await DPOSUser.createOfflineUserAsync(
-      config.ethEndpoint,
-      config.ethPrivateKey,
-      config.dappchainEndpoint,
-      config.dappchainPrivateKey,
-      config.chainId,
-      config.loomGatewayEthAddress,
-        GatewayVersion.SINGLESIG
-    );
+    const user = await createUser(config)
     try {
         console.log('trying to map acc')
       await user.mapAccountsAsync();
         console.log('mapped acc')
     } catch (err) {
-    console.log("got err")
       console.error(err);
     }
   });
@@ -160,15 +133,7 @@ program
   .command("list-validators")
   .description("Show the current DPoS validators")
   .action(async function() {
-    const user = await DPOSUser.createOfflineUserAsync(
-      config.ethEndpoint,
-      config.ethPrivateKey,
-      config.dappchainEndpoint,
-      config.dappchainPrivateKey,
-      config.chainId,
-      config.loomGatewayEthAddress,
-        GatewayVersion.SINGLESIG
-    );
+    const user = await createUser(config)
     try {
       const validators = await user.listValidatorsAsync();
       console.log(`Current validators:`);
@@ -192,15 +157,7 @@ program
   .command("list-candidates")
   .description("Show the current DPoS candidates (along with their metadata)")
   .action(async function() {
-    const user = await DPOSUser.createOfflineUserAsync(
-      config.ethEndpoint,
-      config.ethPrivateKey,
-      config.dappchainEndpoint,
-      config.dappchainPrivateKey,
-      config.chainId,
-      config.loomGatewayEthAddress,
-        GatewayVersion.SINGLESIG
-    );
+    const user = await createUser(config)
     try {
       const candidates = await user.listCandidatesAsync();
       console.log(`Current candidates:`);
@@ -225,15 +182,7 @@ program
   .option("-v, --validator <dappchain b64 address>")
   .option("-d, --delegator <dappchain b64 address>")
   .action(async function(option) {
-    const user = await DPOSUser.createOfflineUserAsync(
-      config.ethEndpoint,
-      config.ethPrivateKey,
-      config.dappchainEndpoint,
-      config.dappchainPrivateKey,
-      config.chainId,
-      config.loomGatewayEthAddress,
-        GatewayVersion.SINGLESIG
-    );
+    const user = await createUser(config)
     try {
       const delegation = await user.checkDelegationsAsync(
         option.validator,
@@ -259,15 +208,7 @@ program
   .description("Get back the user rewards")
   .option("-a, --account <account to withdraw the rewards to>")
   .action(async function() {
-    const user = await DPOSUser.createOfflineUserAsync(
-      config.ethEndpoint,
-      config.ethPrivateKey,
-      config.dappchainEndpoint,
-      config.dappchainPrivateKey,
-      config.chainId,
-      config.loomGatewayEthAddress,
-        GatewayVersion.SINGLESIG
-    );
+    const user = await createUser(config)
     try {
       const rewards = await user.claimDelegationsAsync();
       console.log(`User claimed back rewards: ${rewards}`);
@@ -280,15 +221,7 @@ program
   .command("delegate <amount> <validator> <tier>")
   .description("Delegate `amount` to a candidate / validator")
   .action(async function(amount: string, validator: string, tier: string) {
-    const user = await DPOSUser.createOfflineUserAsync(
-      config.ethEndpoint,
-      config.ethPrivateKey,
-      config.dappchainEndpoint,
-      config.dappchainPrivateKey,
-      config.chainId,
-      config.loomGatewayEthAddress,
-      GatewayVersion.SINGLESIG
-    );
+    const user = await createUser(config)
     try {
       const actualAmount = new BN(amount).mul(coinMultiplier);
       console.log(`Delegating ${actualAmount.toString()} to validator`);
@@ -303,15 +236,7 @@ program
   .command("undelegate <amount> <validator>")
   .option("-v, --validator <dappchain b64 address>")
   .action(async function(amount: string, validator: string) {
-    const user = await DPOSUser.createOfflineUserAsync(
-      config.ethEndpoint,
-      config.ethPrivateKey,
-      config.dappchainEndpoint,
-      config.dappchainPrivateKey,
-      config.chainId,
-      config.loomGatewayEthAddress,
-      GatewayVersion.SINGLESIG
-    );
+    const user = await createUser(config)
     try {
       await user.undelegateAsync(validator, new BN(amount).mul(coinMultiplier));
       console.log(`Undelegated ${amount} LOOM to ${validator}`);
@@ -332,16 +257,8 @@ program
     "Account address"
   )
   .action(async function(options) {
+    const user = await createUser(config)
     try {
-      const user = await DPOSUser.createOfflineUserAsync(
-        config.ethEndpoint,
-        config.ethPrivateKey,
-        config.dappchainEndpoint,
-        config.dappchainPrivateKey,
-        config.chainId,
-        config.loomGatewayEthAddress,
-        GatewayVersion.SINGLESIG
-      );
       const balance = await user.getDAppChainBalanceAsync(options.account);
       console.log(`The account's balance is ${balance}`);
     } catch (err) {
@@ -353,17 +270,8 @@ program
   .command("my-delegations")
   .description("display the user's delegations to all candidates")
   .action(async function() {
+    const user = await createUser(config)
     try {
-      const user = await DPOSUser.createOfflineUserAsync(
-        config.ethEndpoint,
-        config.ethPrivateKey,
-        config.dappchainEndpoint,
-        config.dappchainPrivateKey,
-        config.chainId,
-        config.loomGatewayEthAddress,
-        GatewayVersion.SINGLESIG
-      );
-
       const candidates = await user.listCandidatesAsync();
       for (let i in candidates) {
         const c = candidates[i];
